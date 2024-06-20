00c3157e12d3af1aadf28ba6bcdf9c1d2939bd41
kimhyunso <gustn9515@gmail.com>
2024-06-20 17:51:53 +0900

    feat:read-csv 수정

--- routers/csv/read-csv.js
+++ routers/csv/read-csv.js
@@ -20,41 +20,43 @@ const initKafka = async () => {
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage })
 
+const sendProcuder = async (partitionIndex, result) => {
+	await producer.send({
+		topic: 'topic2',
+		messages: [
+			{ value: JSON.stringify(result), partition: partitionIndex % 3 },
+		],
+	})
+}
+
+const matrixResult = (splitFiles) => {
+	let x = splitFiles[4]
+	let y = splitFiles[5]
+	let matrix = []
+
+	matrix.push(x)
+	matrix.push(y)
+
+	return matrix
+}
+
 router.post('/', upload.single('list.csv'), async (req, res, next) => {
 	
 	const fileContent = req.file.buffer.toString('utf-8')
 	const rows = fileContent.split('\r\n')
 	rows.shift()
-	
+	const splitNum = 100
 	const result = []
-	// 0 ~ 4999
-	const partitiondata1 = []
-	// 5000 ~ 9999
-	const partitiondata2 = []
-	// 10000 ~ 14999
-	const partitiondata3 = []
-	// 15000 ~ 19999
-	const partitiondata4 = []
-	// 20000 ~ 나머지
-	const partitiondata5 = []
 
-	for (let i = 0; i < rows.length; i++){
+	for (let i = 0; i < rows.length / splitNum; i++) {
+		
+		for (let j = splitNum * i; j < splitNum * (i + 1); j++){
+			let splitfile = rows[i].split(',')
+			result.push(matrixResult(splitfile))
+		}	
 
-		let splitfile = rows[i].split(',')
-		let x = splitfile[4]
-		let y = splitfile[5]
-		let matrix = []
-		matrix.push(x)
-		matrix.push(y)
-
-		result.push(matrix)
+		await sendProcuder(i, result)
 	}
-	await producer.send({
-		topic: 'topic1',
-		messages: [
-			{ value: JSON.stringify(result), partition: 0},
-		],
-	})
 
 	res.send('value : ' + JSON.stringify(result))
 })
